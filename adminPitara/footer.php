53c7c73680119835271f447a9b06c4db095aff2e
yogeshk96 <yogesh@gadgetronica.com>
2014-05-05 14:14:11 +0530

    adminPitara

---
+++ adminPitara/footer.php
@@ -0,0 +1,30 @@
+<!-- start: MAIN JAVASCRIPTS -->
+		<!--[if lt IE 9]>
+		<script src="assets/plugins/respond.min.js"></script>
+		<script src="assets/plugins/excanvas.min.js"></script>
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+		<![endif]-->
+		<!--[if gte IE 9]><!-->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+		<!--<![endif]-->
+		<script src="assets/plugins/jquery-ui/jquery-ui-1.10.2.custom.min.js"></script>
+		<script src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+		<script src="assets/plugins/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js"></script>
+		<script src="assets/plugins/blockUI/jquery.blockUI.js"></script>
+		<script src="assets/plugins/iCheck/jquery.icheck.min.js"></script>
+		<script src="assets/plugins/perfect-scrollbar/src/jquery.mousewheel.js"></script>
+		<script src="assets/plugins/perfect-scrollbar/src/perfect-scrollbar.js"></script>
+		<script src="assets/plugins/less/less-1.5.0.min.js"></script>
+		<script src="assets/plugins/jquery-cookie/jquery.cookie.js"></script>
+		<script src="assets/plugins/bootstrap-colorpalette/js/bootstrap-colorpalette.js"></script>
+		<script src="assets/js/main.js"></script>
+		<!-- end: MAIN JAVASCRIPTS -->
+		<!-- start: JAVASCRIPTS REQUIRED FOR THIS PAGE ONLY -->
+		
+		<!-- end: JAVASCRIPTS REQUIRED FOR THIS PAGE ONLY -->
+		<script>
+			jQuery(document).ready(function() {
+				Main.init();
+				
+			});
+		</script>
---
+++ adminPitara/include/constants.php
@@ -0,0 +1,35 @@
+<?php
+
+# Databse Infomation
+
+// Database Server (localhost)
+define("DBHOST","localhost");
+// Database Username
+define("DBUSER","teamdp_pitara1"); 
+// Database Password
+define("DBPASS","Pitara123!");                           
+// Database Name
+define("DBNAME","teamdp_pitara1");                     
+// Database Tabel
+define("DBTBLE","users_login");                          
+
+# Location Infomation
+
+// Path of script with trailing slashes
+define("Script_Path","/dealspitara/dpv2/");
+// URL of script (no trailing slash)
+define("Script_URL","http://localhost/dealspitara/dpv2/");
+
+# Session and Cookie Infomation
+
+// Session Lifetimr in Seconds
+define("Session_Lifetime", 60*60);              
+// Cookie names
+define("CKIEUS","USERNAME");              
+define("CKIEPS","PASSWORDMD5");              
+
+# System Settings
+// Require admin approvial for new users
+define("Admin_Approvial", true); // true or false
+
+?>
